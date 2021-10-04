9f16b7d756796fb527914b2033c037e4f04a83c1
Oran <oranwald@gmail.com>
2021-10-04 15:41:00 +0100

    LogFirstLocation

--- MetOfficeAPI.js
+++ MetOfficeAPI.js
@@ -1,12 +1,15 @@
 const got = require("got");
 
 async function index(){
-    const body = await got
-    .get(
+    got.get(
         "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=06677d83-572f-4514-94c1-ef214268b6fd"
     )
-    .json();
-return 4;
+    .json()
+    .then(function(body){
+    console.log(body.Locations.Location[0])
+        return 4
+    })
 }
 
 index();
+
