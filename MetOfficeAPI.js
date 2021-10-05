fdcf87860a173ebdacb08dc156b5d78b18e44093
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-05 15:12:25 +0100

    now searches ID's

--- MetOfficeAPI.js
+++ MetOfficeAPI.js
@@ -2,28 +2,35 @@ const got = require("got");
 
 const tempName = "Ardlui"
 
+
 async function index(){
     got.get(
         "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=06677d83-572f-4514-94c1-ef214268b6fd"
     )
     .json()
+    
     .then(function(body){
         let names = []
         for (let a of body.Locations.Location){
             names.push(a) // pushes to empty array names
         }
+        var filteredArray = body.Locations.Location.filter((obj)=> {
+            if (obj.id==="3068") {
+                console.log(obj)
+                return true;
+                }
+            return false;
+        })
         return names;
-
     })
-    .then(function(names) {
-        for (const a of names){
-            //console.log(a["name"])
-            if (tempName.toLowerCase() === a["name"].toLowerCase()){
-                console.log(a)
-            }
-        }
-    })
+//     .then(function(names) {
+//         for (const a of names){
+//             //console.log(a["name"])
+//             if (tempName.toLowerCase() === a["name"].toLowerCase()){
+//                 // console.log(a)
+//             }
+//         }
+//     })
 }
 
 index()
-
