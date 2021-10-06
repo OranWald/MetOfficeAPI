ddcc7e77929f7febd0195a0ecfc397606ef06a25
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-06 15:29:20 +0100

    myApp started

--- MetOfficeAPI.js
+++ MetOfficeAPI.js
@@ -3,42 +3,8 @@ const got = require("got");
 const tempName = "Ardlui"
 
 const testId = "3068"
-
 async function index(){
-    got.get(
-        "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=06677d83-572f-4514-94c1-ef214268b6fd"
-    )
-    .json()
     
-    .then(function(body){
-        let names = []
-        for (let a of body.Locations.Location){
-            names.push(a) 
-        }
-        var foundLocation = body.Locations.Location.find((obj)=> {
-            if (obj.id=== testId) {
-                return true;
-                }
-            return false;
-        }) ;
-        return got.get(
-            "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" + testId + "?res=3hourly&key=06677d83-572f-4514-94c1-ef214268b6fd"
-        ).json();
-       
-    })
-    .then(function(newBody){
-        console.log(newBody.SiteRep.DV.Location.Period[0].Rep[0])
-    })
-
-
-
-
-
-
-
-
-
-
 
 //     .then(function(names) {
 //         for (const a of names){
--- myApp.js
+++ myApp.js
@@ -1,13 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const got = require("got");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port,  => {
-  console.log(``)
-})
+const readline = require("readline");
 
 
+app.get("/forecast/:userLocation", function (req, res) {
+  got
+    .get(
+      "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=06677d83-572f-4514-94c1-ef214268b6fd"
+    )
+    .json()
+
+    .then(function (body) {
+      let names = [];
+      for (let a of body.Locations.Location) {
+        names.push(a);
+      }
+      for (let i = 0; i < names.length ; i++){
+        if (names[i].names===req.params.Location) {
+          return true
+        }
+        return false;
+      };
+      })
+      return got
+        .get(
+          "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" +
+            testId +
+            "?res=3hourly&key=06677d83-572f-4514-94c1-ef214268b6fd"
+        )
+        .json();
+    });
+
+app.listen(3000);
