f027b6ac49e456e0d9d4f4ddfcdd69eedd338836
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-07 11:26:51 +0100

    completed part 4

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const got = require("got");
-let testId =""
 
 app.get("/forecast/:userLocation", function (req, res) {
   got
@@ -9,22 +8,18 @@ app.get("/forecast/:userLocation", function (req, res) {
       "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=06677d83-572f-4514-94c1-ef214268b6fd"
     )
     .json()
-
     .then(function (body) {
       let names = [];
+      let testId = "";
       for (let a of body.Locations.Location) {
         names.push(a);
       }
-      console.log(req.params.Location)
-      for (let i = 0; i < names.length ; i++){
-        if (names[i].names===req.params.Location) {
-          testId=names[i].id 
-          return testId
+      console.log(req.params.Location);
+      for (let i = 0; i < names.length; i++) {
+        if (names[i].name === req.params.userLocation) {
+          testId = names[i].id;
         }
-        return console.error("bruh, right a proper name.");
-      };
-      
-      })
+      }
       return got
         .get(
           "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" +
@@ -32,6 +27,15 @@ app.get("/forecast/:userLocation", function (req, res) {
             "?res=3hourly&key=06677d83-572f-4514-94c1-ef214268b6fd"
         )
         .json();
+    })
+    .then(function (response) {
+      console.log(response);
+      res.send(response.SiteRep.DV.Location.Period[0]);
+    })
+    .catch(function (ERR) {
+      console.error(ERR);
+      res.send(ERR)
     });
+});
 
 app.listen(3000);
